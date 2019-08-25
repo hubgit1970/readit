b77afdeb8b5a8a09799694dd0748cbcc550fb32c
chrismulraney <searchlumin70@gmail.com>
2019-08-24 20:52:31 -0700

    Add login and logout views

--- readit/urls.py
+++ readit/urls.py
@@ -1,43 +1,51 @@
 """readit URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-
+from django.views.generic import RedirectView
 from django.conf import settings
-from django.conf.urls import url
+from django.conf.urls import include, url
+from django.urls import path
 from django.contrib import admin
-from django.urls import include, path
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
 from books.views import (AuthorDetail, AuthorList, BookDetail, CreateAuthor,
                          list_books, ReviewList, review_book)
 
 urlpatterns = [
+    # Auth
+    url(r'^logout/$', LogoutView.as_view(next_page='books'), name='logout'),
+    url(r'^login/$', LoginView.as_view(template_name='login.html'), name='login'),
+
+    # Admin
     path('admin/', admin.site.urls),
+
+    # Custom
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico')),
     url(r'^$', list_books, name='books'),
     url(r'^authors/$', AuthorList.as_view(), name='authors'),
     url(r'^books/(?P<pk>[-\w]+)/$', BookDetail.as_view(), name='book-detail'),
-    url(r'^authors/add', CreateAuthor.as_view(), name='add-author'),
-    url(r'^authors/(?P<pk>[-\w]+)/$', AuthorDetail.as_view(),
-        name='author-detail'),
-
-    url(r'^review/$', ReviewList.as_view(), name='review-books'),
+    url(r'^authors/add/$', login_required(CreateAuthor.as_view()), name='add-author'),
+    url(r'^authors/(?P<pk>[-\w]+)/$', AuthorDetail.as_view(), name='author-detail'),
+    url(r'^review/$', login_required(ReviewList.as_view()), name='review-books'),
     url(r'^review/(?P<pk>[-\w]+)/$', review_book, name='review-book'),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
 
-    ] + urlpatterns
+    urlpatterns = [
+                      path('__debug__/', include(debug_toolbar.urls)),
+
+                  ] + urlpatterns
