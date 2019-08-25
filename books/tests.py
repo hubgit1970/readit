22babec7dd15035214e97ead052c9cb28a88d0d8
chrismulraney <searchlumin70@gmail.com>
2019-08-24 22:34:27 -0700

    Add tests demo

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Create your tests here
+
+
+class DemoTest(TestCase):
+    def test_addition(self):
+        self.assertEqual(1 + 1, 3)
